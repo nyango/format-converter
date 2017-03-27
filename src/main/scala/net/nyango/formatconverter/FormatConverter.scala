758477d25c1d2ae207cf4b426a7e53db9d3219e2
nyango <nyango.tech@gmail.com>
2017-03-27 12:20:45 +0900

    converts yaml values to jsons except dates

--- src/main/scala/net/nyango/formatconverter/FormatConverter.scala
+++ src/main/scala/net/nyango/formatconverter/FormatConverter.scala
@@ -5,7 +5,7 @@ import play.api.libs.json._
 
 object FormatConverter {
   def yamlStrToJsValue(yamlStr: String): JsValue = {
-    yamlValueToJsValue(yamlStr.toYaml)
+    yamlValueToJsValue(yamlStr.parseYaml)
   }
 
   def jsValueToYamlStr(json: JsValue): String = {
@@ -13,31 +13,35 @@ object FormatConverter {
   }
 
   def jsValueToYamlValue(json: JsValue): YamlValue = json match {
-    case x: JsObject =>
+    case x: JsObject  =>
       YamlObject(x.fields.map(tpl =>
         (YamlString(tpl._1): YamlValue) -> jsValueToYamlValue(tpl._2)).toMap)
-    case x: JsArray =>
+    case x: JsArray   =>
       YamlArray(x.value.map(jsValueToYamlValue).toVector)
-    case n: JsNumber =>
+    case n: JsNumber  =>
       YamlNumber(n.value)
-    case s: JsString =>
+    case s: JsString  =>
       YamlString(s.value)
     case b: JsBoolean =>
       YamlBoolean(b.value)
-    case _ => YamlNull
+    case _            => YamlNull
   }
 
   def yamlValueToJsValue(yaml: YamlValue): JsValue = yaml match {
-    case x: YamlObject =>
-      JsObject(x.fields.map(tpl => tpl._1.toString -> yamlValueToJsValue(tpl._2)))
-    case x: YamlArray =>
+    case x: YamlObject  =>
+      JsObject(x.fields.collect {
+        case (YamlString(key), v: YamlValue) => key -> yamlValueToJsValue(v)
+      })
+    case x: YamlArray   =>
       JsArray(x.elements.map(yamlValueToJsValue))
-    case n: YamlNumber =>
+    case n: YamlNumber  =>
       JsNumber(n.value)
-    case s: YamlString =>
+    case s: YamlString  =>
       JsString(s.value)
     case b: YamlBoolean =>
       JsBoolean(b.boolean)
-    case _ => JsNull
+    case d: YamlDate => // There is not any "JsDate" in play-json
+      JsString(d.prettyPrint)
+    case _              => JsNull
   }
 }
--- src/test/scala/net/nyango/formatconverter/FormatConverterSpec.scala
+++ src/test/scala/net/nyango/formatconverter/FormatConverterSpec.scala
@@ -1,10 +1,12 @@
 package net.nyango.formatconverter
 
 import org.scalatest.FlatSpec
+import net.jcazevedo.moultingyaml._
+import play.api.libs.json.JsObject
 
 class FormatConverterSpec extends FlatSpec {
 
-  val yaml1 =
+  private val yaml1 =
     """
       |invoice: 34843
       |date   : 2001-01-23
@@ -32,8 +34,36 @@ class FormatConverterSpec extends FlatSpec {
       |total: 4443.52
     """.stripMargin
 
-  "yamlToJson" should "converts correctly" in {
+
+  private val yaml1_without_date =
+    """
+      |invoice: 34843
+      |bill-to: &id001
+      |    given  : Chris
+      |    family : Dumars
+      |    address:
+      |        lines: |
+      |            458 Walkman Dr.
+      |            Suite #292
+      |        city    : Royal Oak
+      |        state   : MI
+      |        postal  : 48046
+      |ship-to: *id001
+      |product:
+      |    - sku         : BL394D
+      |      quantity    : 4
+      |      description : Basketball
+      |      price       : 450.00
+      |    - sku         : BL4438H
+      |      quantity    : 1
+      |      description : Super Hoop
+      |      price       : 2392.00
+      |tax  : 251.42
+      |total: 4443.52
+    """.stripMargin
+
+  "yamlToJson" should "converts correctly, except date" in {
     val jsValue = FormatConverter.yamlStrToJsValue(yaml1)
-    assert(FormatConverter.jsValueToYamlStr(jsValue) == yaml1)
+    assert(FormatConverter.jsValueToYamlStr(jsValue.as[JsObject] - "date").parseYaml == yaml1_without_date.parseYaml)
   }
 }
